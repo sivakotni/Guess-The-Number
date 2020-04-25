e7a108cdc45405fce0b8fec57866a90477ec7cd9
sivakotni <58593595+sivakotni@users.noreply.github.com>
2020-04-25 19:31:50 +0530

    count the number of tries

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from random import randint
-a=randint(0,1000)
+a=randint(0,1)
 b=int(input('guess the number i have taken:'))
+i=0
 while(b!=a):
 	if b>a:
 		b=int(input('you are too high try another:'))
 	else :
 		b=int(input('you are too low try another:'))
-print('jackpot')
+	i+=1
+print('you have won jackpot in {} tries'.format(i))
