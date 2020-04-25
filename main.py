82f64e15b174188a0bc557d92d9e7b49de6f27b4
sivakotni <58593595+sivakotni@users.noreply.github.com>
2020-04-25 19:18:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 from random import randint
 a=randint(0,1000)
 b=int(input('guess the number i have taken:'))
-if b==a:
-	print('jackpot')
 while(b!=a):
 	if b>a:
 		b=int(input('you are too high try another:'))
-		if b==a:
-			print('jackpot')
 	else :
 		b=int(input('you are too low try another:'))
-		if a==b:
-			print('jackpot')
+print('jackpot')
 		
 
 
